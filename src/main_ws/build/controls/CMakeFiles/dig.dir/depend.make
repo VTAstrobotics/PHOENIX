a3778da9e3947a280a5a34a031d4483b052b491c
Henry Mazaika <38223629+H3nryM@users.noreply.github.com>
2024-01-27 14:59:58 -0500

    Init ros (#126)
    * init repo with basic ros stuff
    
    * init repo with basic ros stuff
    
    * add perception pkg skeleton
    
    * add basic package skeleton

---
+++ src/main_ws/build/controls/CMakeFiles/dig.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dig.
+# This may be replaced when dependencies are built.
---
+++ src/main_ws/build/teleop/CMakeFiles/dig.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dig.dir/src/dig.cpp.o"
+  "CMakeFiles/dig.dir/src/dig.cpp.o.d"
+  "dig"
+  "dig.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dig.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

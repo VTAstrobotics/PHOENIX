c24ec2fbf1afa42c84e2bb8df2ffc2a50cffd7d6
H3nryM <38223629+H3nryM@users.noreply.github.com>
2024-01-29 22:01:15 +0000

    format dump but test the workflow uses our custom clang format

--- src/main_ws/src/controls/src/dump.cpp
+++ src/main_ws/src/controls/src/dump.cpp
@@ -4,26 +4,21 @@
 #include "std_msgs/msg/string.hpp"
 using std::placeholders::_1;
 
-class Dump : public rclcpp::Node
-{
-  public:
-    Dump()
-    : Node("dump")
-    {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&Dump::topic_callback, this, _1));
-    }
+class Dump : public rclcpp::Node {      // test
+ public:
+  Dump() : Node("dump") {
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+        "topic", 10, std::bind(&Dump::topic_callback, this, _1));
+  }
 
-  private:
-    void topic_callback(const std_msgs::msg::String & msg) const
-    {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
-    }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+ private:
+  void topic_callback(const std_msgs::msg::String& msg) const {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<Dump>());
   rclcpp::shutdown();

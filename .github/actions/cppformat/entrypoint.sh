cb276ba6cc0746e79a0a82aaaec74d0cbbd65151
Henry Mazaika <henrymazaika@gmail.com>
2024-09-28 23:45:53 +0000

    testing (workflows, sorry for commit spam)

--- .github/actions/cppformat/entrypoint.sh
+++ .github/actions/cppformat/entrypoint.sh
@@ -9,7 +9,8 @@ REPO_FULLNAME=$(jq -r ".repository.full_name" "$GITHUB_EVENT_PATH")
 echo "## Initializing git repo..."
 git init
 echo "### Adding git remote..."
-git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+# git remote add origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
+git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_FULLNAME.git
 echo "### Getting branch"
 BRANCH=${GITHUB_REF#*refs/heads/}
 echo "### git fetch $BRANCH ..."

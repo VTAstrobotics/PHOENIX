559f1062cfbebb655395861b5c0fae4b38d9e175
VaTechAstrobotics <vtastrobotics@gmail.com>
2024-10-11 16:30:46 +0000

    add info msg to entrypoint

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -17,6 +17,7 @@ cd /workspaces/PHOENIX/
 ./build.sh
 
 # Give permissions to input devices, like Xbox controller
+printf "[INFO]: If you get an error like \"chmod: cannot access '/dev/input/js*': No such file or directory\" do not worry. If it ALSO says \"chmod: cannot access '/dev/input/event*': No such file or directory\" AND you have a controller plugged in, there may be an issue with seeing the controller.\n"
 sudo chmod +rx /dev/input/event*
 # this one may be unneeded
 sudo chmod +rx /dev/input/js*

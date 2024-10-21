d1db5ad7fbb6db7685b849110ea81f48df9e5f4f
VaTechAstrobotics <vtastrobotics@gmail.com>
2024-10-21 16:15:52 +0000

    reformatted to make a lil nicer

--- launch.sh
+++ launch.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-echo "Changing directory"
+# Setup
+echo "Setting up..."
 cd /workspaces/PHOENIX/src/main_ws
-
-echo "Sourcing"
 source install/setup.bash
 
-echo "Launching background processes"
+# Running
+echo "Launching nodes..."
 ros2 launch controls launch.yaml &
 ros2 launch teleop launch.yaml &
-
-echo "Starting Joystick control"
 ros2 run joy joy_node
 
+# Teardown
+echo "Shutting down..."
 pkill ros2
 pkill dig
 pkill dump

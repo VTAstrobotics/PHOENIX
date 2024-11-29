8d62678369a8abea8c3275282131c50556b1c58f
LeScooot <jeronimus.ryan@gmail.com>
2024-11-29 18:49:20 +0000

    builds microros

--- build.sh
+++ build.sh
@@ -4,10 +4,20 @@ printf "\nbuild.sh:\n"
 echo "Changing directory"
 cd /workspaces/PHOENIX/src/main_ws
 
-echo "Building project"
+echo "Building main ws"
 colcon build --symlink-install
 
-echo "Sourcing"
+echo "Sourcing main ws"
 source install/setup.bash
+
+cd /workspaces/PHOENIX/micro_ws
+
+echo "Building micro ws"
+cd micro_ros_raspberrypi_pico_sdk
+rm -rf build/
+mkdir build
+cd build
+cmake ..
+make
 
 printf "build.sh done.\n\n"
